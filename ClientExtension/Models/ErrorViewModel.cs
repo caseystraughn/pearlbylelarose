005b13b268919b00e5170be6d69ae85b66424bb9
Russ K <russell@directscale.exigo.com>
2023-04-28 16:19:37 -0600

    Initial commit, with updated DS library and renamed top level namespace.

---
+++ ClientExtension/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PearlClientExtension.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
